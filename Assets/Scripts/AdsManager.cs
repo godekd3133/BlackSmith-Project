c372a4d3ca2ebe17753e52a991dc03a7e5024dbd
MinkyuKim <godekd3133@naver.com>
2021-05-04 00:20:30 +0900

    no message

--- Assets/Scripts/AdsManager.cs
+++ Assets/Scripts/AdsManager.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System.Diagnostics;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using GoogleMobileAds.Api;
@@ -7,15 +8,18 @@ using System;
 
 public class AdsManager : MonoBehaviour
 {
-    private string rewardID = "ca-app-pub-3751333748072855~9583494717";
+    // private string rewardID = "ca-app-pub-3751333748072855~9583494717";
     // 실제 광고 ID
-   // private string rewardTestID = "ca-app-pub-3940256099942544/5224354917";
+
+
+    private string rewardTestID = "ca-app-pub-3940256099942544/5224354917";
     // 테스트 광고 ID, 지금은 테스트를 사용
 
     private RewardedAd rewardedAd;
 
     public AdsManager(RewardedAd rewardedAd)
     {
+
         this.rewardedAd = rewardedAd;
     }
 
@@ -25,19 +29,17 @@ public class AdsManager : MonoBehaviour
 
     void Start()
     {
+        //ca-app-pub-3751333748072855~9583494717
+        MobileAds.Initialize("ca-app-pub-3940256099942544~3347511713");
 
-MobileAds.Initialize("ca-app-pub-3751333748072855~9583494717");
 
 
-        // rewardedAd = new RewardedAd(rewardID);
-        // AdRequest request = new AdRequest.Builder().Build();
-        // rewardedAd.LoadAd(request); // 광고 로드
-
-        // rewardedAd.OnUserEarnedReward += HandleUserEarnedReward;
-        // // 사용자가 광고를 끝까지 시청했을 때
+        //rewardedAd.OnUserEarnedReward += HandleUserEarnedReward;
+        // 사용자가 광고를 끝까지 시청했을 때
         // rewardedAd.OnAdClosed += HandleRewardedAdClosed;
-        // // 사용자가 광고를 중간에 닫았을 때
+        // 사용자가 광고를 중간에 닫았을 때
 
+        print("ASdsa");
         // loadbtn = GameObject.Find("Ruby").GetComponent<Button>();
         // loadbtn.onClick.AddListener(CreateAndLoadRewardedAd);
     }
@@ -52,16 +54,26 @@ MobileAds.Initialize("ca-app-pub-3751333748072855~9583494717");
 
     public void UserChoseToWatchAd()
     {
+        rewardedAd = new RewardedAd(rewardTestID);
+
+        AdRequest request = new AdRequest.Builder().Build();
+        rewardedAd.LoadAd(request); // 광고 로드
+
         if (rewardedAd.IsLoaded()) // 광고가 로드 되었을 때
         {
+
             rewardedAd.Show(); // 광고 보여주기
+        }
+        else
+        {
+            print("안됨");
         }
     }
 
     public void CreateAndLoadRewardedAd() // 광고 다시 로드하는 함수
     {
-        Debug.Log("Good Job Hyun Jin !!");
-        rewardedAd = new RewardedAd(rewardID);
+        ///Debug.Log("Good Job Hyun Jin !!");
+        rewardedAd = new RewardedAd(rewardTestID);
 
         rewardedAd.OnUserEarnedReward += HandleUserEarnedReward;
         rewardedAd.OnAdClosed += HandleRewardedAdClosed;
---
+++ Assets/ShareManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShareManager : MonoBehaviour
+{
+    private const string subject = "원하는 제목";
+    private const string body = "이곳에 url 넣기";
+
+    public void Share()
+    {
+
+#if UNITY_ANDROID && !UNITY_EDITOR
+		using (AndroidJavaClass intentClass = new AndroidJavaClass("android.content.Intent")) 
+		using (AndroidJavaObject intentObject = new AndroidJavaObject("android.content.Intent")) {
+			intentObject.Call<AndroidJavaObject>("setAction", intentObject.GetStatic<string>("ACTION_SEND"));
+			intentObject.Call<AndroidJavaObject>("setType", "text/plain");
+			intentObject.Call<AndroidJavaObject>("putExtra", intentObject.GetStatic<string>("EXTRA_SUBJECT"), subject);
+			intentObject.Call<AndroidJavaObject>("putExtra", intentObject.GetStatic<string>("EXTRA_TEXT"), body);
+			using (AndroidJavaClass unity = new AndroidJavaClass("com.unity3d.player.UnityPlayer"))
+			using (AndroidJavaObject currentActivity = unity.GetStatic<AndroidJavaObject>("currentActivity")) 
+			using (AndroidJavaObject jChooser = intentClass.CallStatic<AndroidJavaObject>("createChooser", intentObject, "Share Via"))
+			currentActivity.Call("startActivity", jChooser);
+		}
+#endif
+    }
+}
